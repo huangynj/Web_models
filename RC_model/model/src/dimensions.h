67fe2b4e2c5cbb65ad1f34c875ff417e71253c9f
Marty Singh <mssingh@nb.mitx.mit.edu>
2013-10-28 21:55:48 +0000

    changed model resolution. Now 24 levels and timestep of 10 minutes

--- RC_model/model/src/dimensions.h
+++ RC_model/model/src/dimensions.h
@@ -7,8 +7,8 @@ c   nqmx est la dimension de la variable traceur q
 c-----------------------------------------------------------------------
 
       INTEGER iim,jjm,llm,ndm
-      PARAMETER (iim= 1,jjm=1,llm=46,ndm=1) ! 25mb
-c      PARAMETER (iim= 1,jjm=1,llm=21,ndm=1) ! 50mb
+C      PARAMETER (iim= 1,jjm=1,llm=46,ndm=1) ! 25mb
+      PARAMETER (iim= 1,jjm=1,llm=24,ndm=1) ! 50mb
       integer nqmx
       parameter (nqmx=3)
 
