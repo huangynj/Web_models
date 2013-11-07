4a87f5bce7370ccec1b5dd20c4b1884bed9859a7
Marty Singh <mssingh@nb.mitx.mit.edu>
2013-11-07 14:52:02 +0000

    have working batch run script

--- RC_model/python/batch_run.py
+++ RC_model/python/batch_run.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python
 
+# This script runs the RC model.
+# Can be used to prime the cache
+#
+
+
 
 import urllib2
 import urllib 
 import json 
 
-form_url = 'https://eaps.mitx.mit.edu/rc'
+form_url = 'http://eaps-prod.mitx.mit.edu/rc'
 
-
+# These are the default values - change them below
 jdata = {
-                      "days"      :  "100"  ,
-                      "co2"       :  "720"  ,
+                      "days"      :  "50"  ,
+                      "co2"       :  "360"  ,
                       'dry_conv'  :  'on'   , 
                       'n2o'       :  '310'  ,
                       'w_max'     :  '0.0'  ,
@@ -18,7 +23,7 @@ jdata = {
                       'month'     :  '3'    ,
                       'graph_time':  '6'    , 
                       'time_step' :  '10'   ,
-                      'dirname'   :  'temp/tmphuKmMJ', 
+                      'dirname'   :  './batch_run/', 
                       'w_p'       :  '500.0', 
                       'avg_time'  :  '25'   ,
                       'cfc11'     :  '280.0',
@@ -33,7 +38,7 @@ jdata = {
                       'SSTi'      :  '21'   , 
                       'w_top'     :  '100'  , 
                       'w_T'       :  '1000' , 
-                      'alpha'     :  '0.15' , 
+                      'alpha'     :  '0.25' , 
                       'rad_type'  :  '0'    , 
                       'ugust'     :  '5'    ,
                       'rad_freq'  :  '1'    ,
@@ -43,13 +48,19 @@ jdata = {
                       'ml_depth'  :  '1.0'
                    }
 
+# Start any looping here
+
+# Change the parameters like this
+# jdata["days"] = str(500)
+
+
 
 data = urllib.urlencode(jdata)
-print data
-req = urllib2.Request(form_url, data)
+req = urllib2.Request(form_url,data)
+req.add_header("Content-type", "application/x-www-form-urlencoded")
 
 response = urllib2.urlopen(req)
-print response
+print response.read()
 
 
 
